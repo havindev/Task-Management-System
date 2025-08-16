f0edc0992631c89b823ee77bdd06418516d50c5d
havindev <havinworkspace@gmail.com>
2025-08-16 17:59:09 +0700

    Update tasks.js

--- api/tasks.js
+++ api/tasks.js
@@ -1,14 +1,10 @@
 import { kv } from '@vercel/kv';
+import { setCORSHeaders, handlePreflight } from '../utils/cors.js';
 
 export default async function handler(req, res) {
-  // Enable CORS
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
+  setCORSHeaders(res);
+  
+  if (handlePreflight(req, res)) return;
 
   try {
     const { userId, taskId } = req.query;
